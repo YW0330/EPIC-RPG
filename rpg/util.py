830408f33487281abca5dbf2d7600740d833b739
YW Chang <andy.yu29@gmail.com>
2023-06-28 03:55:12 +0800

    Integrate the personal information to information.txt

---
+++ rpg/util.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import inspect
+
+
+def get_script_dir(follow_symlinks=True):
+    if getattr(sys, 'frozen', False):
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+
+
+ADTPATH = get_script_dir()
